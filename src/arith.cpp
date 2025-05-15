a162bf85166ddb83635019e5f4f7ef33d4d2b15a
MichalHe <michal.sk.com@gmail.com>
2025-05-15 12:13:39 +0200

    move ACN_Matrix to header

--- src/arith.cpp
+++ src/arith.cpp
@@ -4,6 +4,7 @@
 #include <new>
 #include <ostream>
 #include <iostream>
+#include <vector>
 
 #include "arith.hpp"
 
@@ -33,89 +34,6 @@ std::ostream& operator<<(std::ostream& os, const Algebraic_Complex_Number& numbe
        << number.k << ")";
     return os;
 }
-
-struct ACN_Matrix {
-    u64 height, width;
-    Algebraic_Complex_Number* data = nullptr;
-
-    ACN_Matrix operator*(const ACN_Matrix& other) const {
-        assert(this->width == other.height);
-
-        u64 result_height = this->height;
-        u64 result_width  = other.width;
-
-        Algebraic_Complex_Number* result_data = new Algebraic_Complex_Number[result_height*result_width];
-        u64 target_cell_idx = 0;
-
-        for (u64 row_idx = 0; row_idx < this->height; row_idx++) {
-            for (u64 col_idx = 0; col_idx < other.width; col_idx++) {
-
-                Algebraic_Complex_Number dot_product = acn_zero();
-                for (u64 elem_idx = 0; elem_idx < this->width; elem_idx++) {
-                    Algebraic_Complex_Number fragment = this->at(row_idx, elem_idx) * other.at(elem_idx, col_idx);
-                    dot_product += fragment;
-                }
-
-                result_data[target_cell_idx] = dot_product;
-                target_cell_idx += 1;
-            }
-        }
-
-        return ACN_Matrix {
-            .height = result_height,
-            .width  = result_width,
-            .data   = result_data
-        };
-    }
-
-    Algebraic_Complex_Number& at(u64 row_idx, u64 col_idx) const {
-        return this->data[row_idx*this->width + col_idx];
-    }
-
-    u64 find_nonzero_elem_in_row(u64 row_idx) const {
-        assert (row_idx < this->height);
-
-        for (u64 elem_idx = 0; elem_idx < this->width; elem_idx++) {
-            Algebraic_Complex_Number& elem = this->at(row_idx, elem_idx);
-            if (!elem.is_zero()) {
-                return elem_idx;
-            }
-        }
-
-        return this->width + 1;
-    }
-
-    void insert_row_at(const ACN_Matrix& row, u64 row_idx, bool skip_shifting_subsequent_rows = false) {
-        assert(row.width == this->width);
-
-        if (!skip_shifting_subsequent_rows) {
-            u64 last_elem_idx  = (this->height - 1) * this->width - 1;
-            u64 first_elem_idx = row_idx * this->width;
-
-            for (u64 elem_idx = last_elem_idx; elem_idx >= first_elem_idx; elem_idx--) {
-                this->data[elem_idx + this->width] = this->data[elem_idx];
-            }
-        }
-
-        for (u64 elem_idx = 0; elem_idx < row.width; elem_idx++) {
-            this->data[row_idx*this->width] = row.at(0, elem_idx);
-        }
-    }
-
-    void subtract_from_ith_row(u64 row_idx, Algebraic_Complex_Number& row_coef, ACN_Matrix& rows_to_subtract, u64 row_to_subtract_idx, Algebraic_Complex_Number& row_to_subtract_coef) const {
-        for (u64 elem_idx = 0; elem_idx < this->width; elem_idx++) {
-            auto subtractee_weighted = this->at(row_idx, elem_idx) * row_coef;
-            auto subtractor_weighted = rows_to_subtract.at(row_idx, elem_idx) * row_to_subtract_coef;
-            auto result_elem = subtractee_weighted - subtractor_weighted;
-
-            this->data[row_idx*this->width + elem_idx] = result_elem;
-        }
-    }
-
-    ~ACN_Matrix() {
-        if (this->data != nullptr) delete[] this->data;
-    }
-};
 
 
 s64 add_row_to_row_echelon_matrix(ACN_Matrix& matrix, ACN_Matrix& row) {
@@ -152,6 +70,23 @@ s64 add_row_to_row_echelon_matrix(ACN_Matrix& matrix, ACN_Matrix& row) {
 
     assert(false);  // Should be unreachable - the row is indepentent and we insert it, or our matrix has full rank and hence we do not insert it
 }
+
+u64 compute_square_matrix_dim_from_1d_repr(const std::vector<s64>& repr) {
+    u64 dimension = static_cast<s64>(std::sqrt(repr.size()));
+
+    assert(dimension*dimension == repr.size());
+    
+    return dimension;
+}
+
+ACN_Matrix square_acn_matrix_from_ints(const std::vector<s64>& ints) {
+    u64 dim = compute_square_matrix_dim_from_1d_repr(ints);
+
+    ACN_Matrix result = {.height = dim, .width = dim, .data = nullptr};
+
+    return result;
+}
+
 
 Direct_ACN convert_acn_into_direct_repr(const Algebraic_Complex_Number& number) {
     if (number.is_zero()) {
--- src/arith.hpp
+++ src/arith.hpp
@@ -117,6 +117,8 @@ struct Algebraic_Complex_Number {
     }
 };
 
+Algebraic_Complex_Number acn_zero();
+
 /**
  * Represents a complex number as:
  *    (1/2)^k * (a + b*(1/sqrt(2)) + i*c + i*d*(1/sqrt(2)))
@@ -131,7 +133,90 @@ struct Direct_ACN {
 };
 
 std::ostream& operator<<(std::ostream& os, const Direct_ACN& number);
-
 Direct_ACN convert_acn_into_direct_repr(const Algebraic_Complex_Number& number);
 
 
+struct ACN_Matrix {
+    u64 height, width;
+    Algebraic_Complex_Number* data = nullptr;
+
+    ACN_Matrix operator*(const ACN_Matrix& other) const {
+        assert(this->width == other.height);
+
+        u64 result_height = this->height;
+        u64 result_width  = other.width;
+
+        Algebraic_Complex_Number* result_data = new Algebraic_Complex_Number[result_height*result_width];
+        u64 target_cell_idx = 0;
+
+        for (u64 row_idx = 0; row_idx < this->height; row_idx++) {
+            for (u64 col_idx = 0; col_idx < other.width; col_idx++) {
+
+                Algebraic_Complex_Number dot_product = acn_zero();
+                for (u64 elem_idx = 0; elem_idx < this->width; elem_idx++) {
+                    Algebraic_Complex_Number fragment = this->at(row_idx, elem_idx) * other.at(elem_idx, col_idx);
+                    dot_product += fragment;
+                }
+
+                result_data[target_cell_idx] = dot_product;
+                target_cell_idx += 1;
+            }
+        }
+
+        return ACN_Matrix {
+            .height = result_height,
+            .width  = result_width,
+            .data   = result_data
+        };
+    }
+
+    Algebraic_Complex_Number& at(u64 row_idx, u64 col_idx) const {
+        return this->data[row_idx*this->width + col_idx];
+    }
+
+    u64 find_nonzero_elem_in_row(u64 row_idx) const {
+        assert (row_idx < this->height);
+
+        for (u64 elem_idx = 0; elem_idx < this->width; elem_idx++) {
+            Algebraic_Complex_Number& elem = this->at(row_idx, elem_idx);
+            if (!elem.is_zero()) {
+                return elem_idx;
+            }
+        }
+
+        return this->width + 1;
+    }
+
+    void insert_row_at(const ACN_Matrix& row, u64 row_idx, bool skip_shifting_subsequent_rows = false) {
+        assert(row.width == this->width);
+
+        if (!skip_shifting_subsequent_rows) {
+            u64 last_elem_idx  = (this->height - 1) * this->width - 1;
+            u64 first_elem_idx = row_idx * this->width;
+
+            for (u64 elem_idx = last_elem_idx; elem_idx >= first_elem_idx; elem_idx--) {
+                this->data[elem_idx + this->width] = this->data[elem_idx];
+            }
+        }
+
+        for (u64 elem_idx = 0; elem_idx < row.width; elem_idx++) {
+            this->data[row_idx*this->width] = row.at(0, elem_idx);
+        }
+    }
+
+    void subtract_from_ith_row(u64 row_idx, Algebraic_Complex_Number& row_coef, ACN_Matrix& rows_to_subtract, u64 row_to_subtract_idx, Algebraic_Complex_Number& row_to_subtract_coef) const {
+        for (u64 elem_idx = 0; elem_idx < this->width; elem_idx++) {
+            auto subtractee_weighted = this->at(row_idx, elem_idx) * row_coef;
+            auto subtractor_weighted = rows_to_subtract.at(row_idx, elem_idx) * row_to_subtract_coef;
+            auto result_elem = subtractee_weighted - subtractor_weighted;
+
+            this->data[row_idx*this->width + elem_idx] = result_elem;
+        }
+    }
+
+    ~ACN_Matrix() {
+        if (this->data != nullptr) delete[] this->data;
+    }
+};
+
+s64 add_row_to_row_echelon_matrix(ACN_Matrix& matrix, ACN_Matrix& row);
